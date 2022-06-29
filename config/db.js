cc899898c9f32d6629c9210e801c678741ec69a9
Suyash Vikram Singh <suyashvikramsingh2002@gmail.com>
2022-06-29 13:29:16 +0530

    Fixed bugs

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI);
     console.log(`Connected to MongoDB`.underline.bgYellow);
   } catch (error) {
     console.log(error);
